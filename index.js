38a59659031f29356ec9f4578ba52e3728331d8b
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 20:43:52 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * forward-object <https://github.com/doowb/forward-object>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
