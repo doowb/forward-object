f55d82608a5f58b3b4a8e345f28789febfcf894d
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-13 00:39:01 -0400

    adds tests for enumerable/non-enumerable

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 'use strict';
 
+/* deps: mocha */
 var assert = require('assert');
+var makeEnumerable = require('make-enumerable');
 var forward = require('./');
+
 
 describe('foward-object', function () {
   it('should foward all properties', function () {
+    var copy = forward();
     var receiver = {};
     var provider = {
       name: 'provider',
@@ -12,11 +16,52 @@ describe('foward-object', function () {
         return '[' + this.name + '] ' + str.toUpperCase();
       }
     };
-    var receiver = forward(receiver, provider);
+    copy(receiver, provider);
     assert.deepEqual(Object.keys(receiver), Object.keys(provider));
   });
 
+  it('should make copied properties non-enumerable', function () {
+    var copy = forward({enumerable: false});
+    var a = {};
+    var b = {one: 'aaa', two: 'bbb', three: 'ccc'};
+    copy(a, b);
+
+    var foo = makeEnumerable(a);
+
+    assert.deepEqual(Object.keys(a), []);
+    assert.deepEqual(Object.keys(a), []);
+    assert.equal(a.one, 'aaa');
+    assert.equal(a.two, 'bbb');
+    assert.equal(a.three, 'ccc');
+    assert.deepEqual(foo, b);
+  });
+
+  it('should return the receiver object', function () {
+    var copy = forward();
+    var a = {};
+    var b = {one: 'aaa', two: 'bbb', three: 'ccc'};
+    var result = copy(a, b);
+    assert.deepEqual(Object.keys(result), ['one', 'two', 'three']);
+  });
+
+  it('should update the receiver object', function () {
+    var copy = forward();
+    var a = {};
+    var b = {one: 'aaa', two: 'bbb', three: 'ccc'};
+    copy(a, b);
+    assert.deepEqual(Object.keys(a), ['one', 'two', 'three']);
+  });
+
+  it('should omit properties defined on `options.omit`', function () {
+    var copy = forward({enumerable: true, omit: 'three'});
+    var a = {};
+    var b = {one: 'aaa', two: 'bbb', three: 'ccc'};
+    copy(a, b);
+    assert.deepEqual(Object.keys(a), ['one', 'two']);
+  });
+
   it('should foward specified properties', function () {
+    var copy = forward({enumerable: true, keys: ['name', 'upper']});
     var receiver = {};
     var provider = {
       name: 'provider',
@@ -25,11 +70,12 @@ describe('foward-object', function () {
         return '[' + this.name + '] ' + str.toUpperCase();
       }
     };
-    var receiver = forward(receiver, provider, ['name', 'upper']);
+    copy(receiver, provider);
     assert.deepEqual(Object.keys(receiver), ['name', 'upper']);
   });
 
   it('should execute methods with the context of the provider', function () {
+    var copy = forward({enumerable: true, keys: ['name', 'upper']});
     var receiver = {};
     var provider = {
       name: 'provider',
@@ -37,7 +83,7 @@ describe('foward-object', function () {
         return '[' + this.name + '] ' + str.toUpperCase();
       }
     };
-    var receiver = forward(receiver, provider, ['name', 'upper']);
+    copy(receiver, provider);
     receiver.name = 'receiver',
     assert.deepEqual(receiver.name, 'receiver');
     assert.deepEqual(provider.name, 'provider');
